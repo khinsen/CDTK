351ca5bf571ffde57b6d55c1b136f8af852e4054
"Konrad Hinsen ext:(%22) <hinsen@cnrs-orleans.fr>
2007-10-31 18:46:49 +0100

    Improvements for maps
    - New method makePositive() subtracts the smallest value from all points.
    - Map display in VMD can be shifted in space.
    - Patterson maps are shown centered on (0, 0, 0) in VMD.

--- CDTK/Crystal.py
+++ CDTK/Crystal.py
@@ -75,6 +75,11 @@ class Map(object):
         self.x1 = N.arange(n1)/float(n1)
         self.x2 = N.arange(n2)/float(n2)
         self.x3 = N.arange(n3)/float(n3)
+        self.vmd_origin = Vector(0., 0., 0.)
+
+    def makePositive(self):
+        smallest = N.minimum.reduce(N.ravel(self.array))
+        self.array -= smallest
 
     def writeToVMDScript(self, filename, label=None):
         if label is None:
@@ -84,7 +89,7 @@ class Map(object):
         vmd_script.write('mol new\n')
         vmd_script.write('mol volume top "%s" \\\n' % label)
         e1, e2, e3 = self.cell.basisVectors()
-        vmd_script.write('  {0. 0. 0.} \\\n') # Origin
+        vmd_script.write('  {%f %f %f} \\\n' % tuple(self.vmd_origin/Units.Ang))
         vmd_script.write('  {%f %f %f} \\\n' % tuple(e1/Units.Ang))
         vmd_script.write('  {%f %f %f} \\\n' % tuple(e2/Units.Ang))
         vmd_script.write('  {%f %f %f} \\\n' % tuple(e3/Units.Ang))
@@ -153,6 +158,13 @@ class ElectronDensityMap(Map):
 class PattersonMap(Map):
 
     default_label = "Patterson map"
+
+    def __init__(self, cell, n1, n2, n3):
+        Map.__init__(self, cell, n1, n2, n3)
+        e1, e2, e3 = self.cell.basisVectors()
+        # display Patterson maps centered on (0, 0, 0) to
+        # facilitate comparisons
+        self.vmd_origin = -0.5*(e1+e2+e3)
 
     def calculateFromIntensities(self, intensities):
         from CDTK_sf_fft import reflections_to_map
