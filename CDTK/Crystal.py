26586394c70f53604b25a0b602834beef4238701
"Konrad Hinsen ext:(%22) <hinsen@cnrs-orleans.fr>
2007-12-09 14:49:39 +0100

    Docstrings for CDTK.Crystal

--- CDTK/Crystal.py
+++ CDTK/Crystal.py
@@ -1,9 +1,25 @@
+# Description of a crystal
+#
+
+"""
+Description of a crystal
+"""
+
 from Scientific.Geometry import Vector, isVector
 from Scientific import N, LA
 
 class UnitCell(object):
 
+    """
+    Unit cell
+    """
+
     def __init__(self, *parameters):
+        """
+        @param parameters: one of 1) three lattice vectors or
+            2) six numbers: the lengths of the three lattice vectors (a, b, c)
+            followed by the three angles (alpha, beta, gamma).
+        """
         if len(parameters) == 6:
             self.a, self.b, self.c, self.alpha, self.beta, self.gamma = \
                     parameters
@@ -34,30 +50,72 @@ class UnitCell(object):
         self.reciprocal_basis = [Vector(r[0]), Vector(r[1]), Vector(r[2])]
 
     def basisVectors(self):
+        """
+        @return: a list containing the three lattice vectors
+        @rtype: C{list} of C{Scientific.Geometry.Vector}
+        """
         return self.basis
 
     def reciprocalBasisVectors(self):
+        """
+        @return: a list containing the three basis vectors
+                 of the reciprocal lattice
+        @rtype: C{list} of C{Scientific.Geometry.Vector}
+        """
         return self.reciprocal_basis
 
     def cellVolume(self):
+        """
+        @return: the volume of the unit cell
+        @rtype: C{float}
+        """
         e1, e2, e3 = self.basis
         return e1*e2.cross(e3)
 
     def cartesianToFractional(self, vector):
+        """
+        @param vector: a vector in real Cartesian space
+        @type vector: C{Scientific.Geometry.Vector}
+        @return: the vector in fractional coordinates
+        @rtype: C{Scientific.N.array}
+        """
         r1, r2, r3 = self.reciprocal_basis
         return N.array([r1*vector, r2*vector, r3*vector])
 
     def cartesianToFractionalMatrix(self):
+        """
+        @return: the 3x3 conversion matrix from real Cartesian space
+                 coordinates to fractional coordinates
+        """
         return N.array(self.reciprocal_basis)
 
     def fractionalToCartesian(self, array):
+        """
+        @param array: a vector in fractional coordinates
+        @type array: C{Scientific.N.array}
+        @return: the vector in real Cartesian space
+        @rtype: C{Scientific.Geometry.Vector}
+        """
         e1, e2, e3 = self.basis
         return array[0]*e1 + array[1]*e2 + array[2]*e3
 
     def fractionalToCartesianMatrix(self):
+        """
+        @return: the 3x3 conversion matrix from fractional
+                 coordinates to real Cartesian space coordinates
+        """
         return N.transpose(self.basis)
 
     def isCompatibleWith(self, other_cell, precision=1.e-5):
+        """
+        @param other_cell: a unit cell
+        @type other_cell: L{UnitCell} or C{MMTK.Universe}
+        @param precision: the absolute precision of the comparison
+        @type precision: C{float}
+        @return: C{True} if the lattice vectors of the two unit cells differ
+                 by a vector of length < precision
+        @rtype: C{bool}
+        """
         other_basis = other_cell.basisVectors()
         for i in range(3):
             if (other_basis[i]-self.basis[i]).length() > precision:
