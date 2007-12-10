cf2b22f641737d7b1944b326a1765e7c1fdb802c
"Konrad Hinsen ext:(%22) <hinsen@cnrs-orleans.fr>
2007-12-10 09:56:40 +0100

    Docstrings for CDTK.Maps

--- CDTK/Maps.py
+++ CDTK/Maps.py
@@ -1,10 +1,35 @@
+# Electron density maps and Patterson maps
+#
+
+"""
+Electron density maps and Patterson maps
+"""
+
 from CDTK import Units
-from Scientific.Geometry import Vector, isVector, delta
+from Scientific.Geometry import Vector, isVector
+import Scientific.Geometry
 from Scientific import N, LA
 
 class Map(object):
 
+    """
+    Map base class
+    """
+
     def __init__(self, cell, n1, n2, n3):
+        """
+        @param cell: the unit cell for which the map is defined
+        @type cell: L{CDTK.Crystal.UnitCell}
+        @param n1: the number of points in the grid along the
+                   first lattice vector
+        @type n1: C{int}
+        @param n2: the number of points in the grid along the
+                   second lattice vector
+        @type n2: C{int}
+        @param n3: the number of points in the grid along the
+                   third lattice vector
+        @type n3: C{int}
+        """
         self.cell = cell
         self.array = N.zeros((n1, n2, n3), N.Float)
         self.shape = (n1, n2, n3)
@@ -14,10 +39,20 @@ class Map(object):
         self.vmd_origin = Vector(0., 0., 0.)
 
     def makePositive(self):
+        """
+        Subtract the smallest map value from all other map values,
+        such that the smallest value becomes 0.
+        """
         smallest = N.minimum.reduce(N.ravel(self.array))
         self.array -= smallest
 
     def writeToVMDScript(self, filename, label=None):
+        """
+        @param filename: the name of the generated VMD script
+        @type filename: C{string}
+        @param label: the label of the map as displayed by VMD
+        @type label: C{string}
+        """
         if label is None:
             label = self.default_label
         factor = 1./N.maximum.reduce(N.ravel(self.array))
@@ -42,9 +77,37 @@ class Map(object):
 
 class ElectronDensityMap(Map):
 
+    """
+    Electron density map
+
+    An electron density map can be calculated from a StructureFactor
+    by Fourier transform or directly from an atomic model.
+    """
+
     default_label = "Electron density"
 
     def calculateFromUnitCellAtoms(self, atom_iterator, cell=None):
+        """
+        @param atom_iterator: an iterator or sequence that yields
+                              for each atom in the unit cell a
+                              tuple of (chemical element,
+                              position vector, position fluctuation,
+                              occupancy). The position fluctuation
+                              can be a symmetric tensor (ADP tensor)
+                              or a scalar (implicitly multiplied by
+                              the unit tensor).
+        @type atom_iterator: iterable
+        @param cell: a unit cell, which defaults to the unit cell for
+                     which the map object is defined. If a different
+                     unit cell is given, the map is calculated for
+                     this cell in fractional coordinates and converted
+                     to Cartesian coordinates using the unit cell of
+                     the map object. This is meaningful only if the two
+                     unit cells are very similar, such as for unit cells
+                     corresponding to different steps in a constant-pressure
+                     Molecular Dynamics simulation.
+        @type cell: L{CDTK.Crystal.UnitCell}
+        """
         if cell is None:
             cell = self.cell
         m_fc = cell.fractionalToCartesianMatrix()
@@ -62,9 +125,9 @@ class ElectronDensityMap(Map):
             dx3 += (dx3 < -0.5).astype(N.Int) - (dx3 >= 0.5).astype(N.Int)
             for i in range(5):
                 if isinstance(adp, float):
-                    sigma = (adp + bdiv[i])*delta
+                    sigma = (adp + bdiv[i])*Scientific.Geometry.delta
                 else:
-                    sigma = adp + bdiv[i]*delta
+                    sigma = adp + bdiv[i]*Scientific.Geometry.delta
                 sigma_inv = LA.inverse(sigma.array)
                 weight = a[i] * N.sqrt(LA.determinant(sigma_inv)) * occupancy
                 m = -0.5*N.dot(N.transpose(m_fc), N.dot(sigma_inv, m_fc))
@@ -81,6 +144,15 @@ class ElectronDensityMap(Map):
                 N.add(self.array, weight*N.exp(e), self.array)
 
     def calculateFromUniverse(self, universe, adps, conf=None):
+        """
+        @param universe: a periodic MMTK universe
+        @type universe: C{MMTK.Periodic3DUniverse}
+        @param adps: the anisotropic displacement parameters for all atoms
+        @type adps: C{MMTK.ParticleTensor}
+        @param conf: a configuration for the universe, defaults to the
+                     current configuration
+        @type conf: C{MMTK.Configuration}
+        """
         if conf is None:
             conf = universe.configuration()
         cell = universe.__class__()
@@ -91,6 +163,10 @@ class ElectronDensityMap(Map):
                                         cell)
 
     def calculateFromStructureFactor(self, sf):
+        """
+        @param sf: a structure factor set
+        @type sf: L{CDTK.Reflections.StructureFactor}
+        """
         from CDTK_sf_fft import reflections_to_map
         from CDTK.Reflections import StructureFactor
         if not isinstance(sf, StructureFactor):
@@ -103,6 +179,12 @@ class ElectronDensityMap(Map):
 
 class PattersonMap(Map):
 
+    """
+    Patterson map
+
+    A Patterson map can be calculated from reflection intensities.
+    """
+
     default_label = "Patterson map"
 
     def __init__(self, cell, n1, n2, n3):
@@ -113,6 +195,10 @@ class PattersonMap(Map):
         self.vmd_origin = -0.5*(e1+e2+e3)
 
     def calculateFromIntensities(self, intensities):
+        """
+        @param intensities: a set of reflection intensities
+        @type intensities: L{CDTK.Reflections.IntensityData}
+        """
         from CDTK_sf_fft import reflections_to_map
         from CDTK.Reflections import IntensityData
         if not isinstance(intensities, IntensityData):
