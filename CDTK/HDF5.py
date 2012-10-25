f9040bb8654f5c99991546fa4d0294e177f59d39
Konrad Hinsen <konrad.hinsen@cnrs-orleans.fr>
2012-10-25 15:57:40 +0200

    Use weakrefs for caching

--- CDTK/HDF5.py
+++ CDTK/HDF5.py
@@ -15,6 +15,7 @@ translated to and from HDF5 object references.
 """
 
 import h5py
+import weakref
 
 # For now, import all modules directly for hard-wired handlers
 from CDTK.Reflections import FrozenReflectionSet
@@ -34,8 +35,9 @@ class HDF5Store(object):
             if file_mode is None:
                 file_mode = 'r'
             self.root = h5py.File(hdf5_group_or_filename, file_mode)
-        self.cache = {}
-        self.info_cache = {}
+        self.path_cache = weakref.WeakKeyDictionary()
+        self.obj_cache = weakref.WeakValueDictionary()
+        self.info_cache = weakref.WeakKeyDictionary()
 
     def __enter__(self):
         return self
@@ -60,13 +62,14 @@ class HDF5Store(object):
             path = "_UNNAMED_/%d" % id(obj)
         path = self.relativePath(path)
 
-        if id(obj) in self.cache:
-            if path != self.cache[id(obj)]:
+        cached_path = self.path_cache.get(obj, None)
+        if cached_path is not None:
+            if path != cached_path:
                 if not path.startswith("_UNNAMED_"):
                     # add a soft link
-                    self.root[path] = h5py.SoftLink(self.cache[id(obj)])
-            node = self.root[self.cache[id(obj)]]
-            info = self.info_cache.get(id(obj), None)
+                    self.root[path] = h5py.SoftLink(cached_path)
+            node = self.root[cached_path]
+            info = self.info_cache.get(obj, None)
             return node, info
 
         ret = obj.storeHDF5(self, path)
@@ -76,9 +79,9 @@ class HDF5Store(object):
         else:
             node = ret
             info = None
-        self.cache[path] = obj
-        self.cache[id(obj)] = path
-        self.info_cache[id(obj)] = info
+        self.obj_cache[path] = obj
+        self.path_cache[obj] = path
+        self.info_cache[obj] = info
         return node, info
 
     def retrieve(self, path_or_node_or_ref):
@@ -93,7 +96,7 @@ class HDF5Store(object):
             raise TypeError('path_or_node_or_ref must be a string, '
                             'an HDF5 node, or an HDF5 reference')
         path = self.relativePath(path)
-        data = self.cache.get(path, None)
+        data = self.obj_cache.get(path, None)
         if data is not None:
             return data
         link_obj = self.root.get(path, getlink=True)
@@ -104,6 +107,6 @@ class HDF5Store(object):
         if data_class is None:
             raise ValueError("unknown node type at " + path)
         obj = self.handlers[data_class](self, node)
-        self.cache[path] = obj
-        self.cache[id(obj)] = path
+        self.obj_cache[path] = obj
+        self.path_cache[obj] = path
         return obj
